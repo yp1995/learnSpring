14ce9b8ba9108b730cad0118d9aea53c1827d9a1
yp1995 <13347972028@163.com>
2020-04-01 13:41:44 +0800

    修改变量â

--- src/main/java/com/example/demo/controller/AuthorityController.java
+++ src/main/java/com/example/demo/controller/AuthorityController.java
@@ -4,6 +4,7 @@ import com.example.demo.dto.AccessTokenDTO;
 import com.example.demo.dto.GithubUser;
 import com.example.demo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,14 +14,20 @@ import org.thymeleaf.util.StringUtils;
 public class AuthorityController {
     @Autowired
     private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("9067c48af2a4b634a80d");
-        accessTokenDTO.setClient_secret("4a63610871d0e3d49b80600623490e7cca07569f");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8877/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         if (!StringUtils.isEmpty(accessToken)){
